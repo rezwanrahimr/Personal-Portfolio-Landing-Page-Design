c88a240c32ce3dad1f70e3d46a91a7fde1b19329
Rezwan Rahim <rezwanrahim69@gmail.com>
2024-09-20 16:40:54 +0600

    added about section and make it responsive

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-const hamburger = document.querySelector('hamburger');
-const navMenu = document.querySelector('nav_menu');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav_menu');
 
 hamburger.addEventListener('click', () => {
-    console.log('clicked');
     navMenu.classList.toggle('active');
 })
-
-console.log('script loaded');
