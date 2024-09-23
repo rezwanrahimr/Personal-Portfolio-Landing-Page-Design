f8c07fe1430eed0d586f77f949dc5d41c3d60f42
rezwanrahimr <rezwanrahim69@gmail.com>
2024-09-23 16:37:31 +0600

    update mobile responsive code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
+const menuIcon = document.getElementById('menu_icon');
 const navMenu = document.querySelector('.nav_menu');
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
+
+    if (navMenu.classList.contains('active')) {
+        menuIcon.classList.replace('fa-bars', 'fa-times'); // Switch to close icon
+    } else {
+        menuIcon.classList.replace('fa-times', 'fa-bars'); // Switch back to hamburger
+    }
+
 })
+
